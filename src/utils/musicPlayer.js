1a6f92e429043652f0542f72add0549253c59956
lulusabee <lulusabeetw@gmail.com>
2021-03-26 00:40:27 +0800

    新增成功失敗大小遊戲音樂

--- src/utils/musicPlayer.js
+++ src/utils/musicPlayer.js
@@ -3,6 +3,7 @@ import {musicData} from '../data.source';
 
 Sound.setCategory('Playback');
 
+//Backgaeground
 var backgroundMusic;
 export function playerBackgroundMusic() {
   backgroundMusic = new Sound(musicData.background, (error) => {
@@ -23,6 +24,7 @@ export function pauseBackgroundMusic() {
   backgroundMusic.pause();
 }
 
+//On botton click
 var btnClickMedia = new Sound(musicData.click, (error) => {
   if (error) {
     console.log('failed to load the btnClickMedia\n', error);
@@ -35,4 +37,67 @@ var btnClickMedia = new Sound(musicData.click, (error) => {
 export function playBtnClickMedia() {
   btnClickMedia.stop();
   btnClickMedia.play();
+}
+
+//LittleGame
+var littleGameSuccess = new Sound(musicData.gameSuccess, (error) => {
+  if (error) {
+    console.log('failed to load the littleGameSuccess Success\n', error);
+    return;
+  }
+  // loaded successfully
+  console.log('littleGameSuccess music loaded successfully.');
+});
+var littleGameFail = new Sound(musicData.gameFail, (error) => {
+  if (error) {
+    console.log('failed to load the littleGameFail Fail\n', error);
+    return;
+  }
+  // loaded Failfully
+  console.log('littleGameFail music loaded successfully.');
+});
+
+export function playGameSuccess() {
+  littleGameSuccess.stop();
+  littleGameSuccess.play();
+}
+
+export function playGameFail() {
+  littleGameFail.stop();
+  littleGameFail.play();
+}
+
+//Game(entire)
+var gameSuccess = new Sound(musicData.success, (error) => {
+  if (error) {
+    console.log('failed to load the gameSuccess Success\n', error);
+    return;
+  }
+  // loaded successfully
+  console.log('gameSuccess music loaded successfully.');
+});
+var gameFail = new Sound(musicData.fail, (error) => {
+  if (error) {
+    console.log('failed to load the gameFail Fail\n', error);
+    return;
+  }
+  // loaded Failfully
+  console.log('gameFail music loaded successfully.');
+});
+export function playSuccess() {
+  gameSuccess.play();
+  releaseMemory();
+}
+
+export function playFail() {
+  gameSuccess.play();
+  releaseMemory();
+}
+
+function releaseMemory() {
+  btnClickMedia.release();
+  littleGameSuccess.release();
+  littleGameFail.release();
+  gameSuccess.release();
+  gameFail.release();
 }
