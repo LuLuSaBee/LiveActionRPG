1b52fcdba2db31160cd717df99c25a72b9243bc1
lulusabee <lulusabeetw@gmail.com>
2021-03-27 13:04:28 +0800

    改進紀錄頁載入效率

--- src/Pages/StoryRecordPage.js
+++ src/Pages/StoryRecordPage.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {View, ScrollView, Image, Text, TouchableOpacity} from 'react-native';
+import {View, FlatList, Image, Text, TouchableOpacity} from 'react-native';
 import Styles from '../Styles/StoryRecordPage.style';
 import InfoBox from '../Views/Elements/InfoBox';
 import {pop} from '../utils/routerAction';
@@ -16,40 +16,51 @@ class StoryRecordPage extends React.Component {
   render() {
     return (
       <View style={Styles.page}>
-        <ScrollView style={Styles.container}>
-          {this.props.storyRecord.map((record) => (
-            <InfoBox
-              key={record.time}
-              boxStyle={[Styles.box]}
-              content={[
-                <View key="img" style={Styles.imageContainer}>
-                  <Image
-                    style={Styles.image}
-                    source={npcData[record.npcID].img}
-                  />
-                </View>,
-                <View key="info" style={Styles.infoContainer}>
-                  <View style={Styles.nameContainer}>
-                    <Text style={Styles.name}>
-                      {npcData[record.npcID].name}
-                    </Text>
-                  </View>
-                  <View style={Styles.linesContainer}>
-                    <Text>{record.line}</Text>
-                  </View>
-                  <View style={Styles.timeContainer}>
-                    <Text>
-                      {moment(
-                        record.time.seconds * 1000 +
-                          record.time.nanoseconds / 1000000,
-                      ).format('HH:mm:ss')}
-                    </Text>
-                  </View>
-                </View>,
-              ]}
-            />
-          ))}
-        </ScrollView>
+        <FlatList
+          style={Styles.container}
+          data={this.props.storyRecord}
+          keyExtractor={(item) =>
+            (
+              item.time.seconds * 1000 +
+              item.time.nanoseconds * 1000000
+            ).toString()
+          }
+          renderItem={(item) => {
+            const record = item.item;
+            return (
+              <InfoBox
+                key={record.time}
+                boxStyle={[Styles.box]}
+                content={[
+                  <View key="img" style={Styles.imageContainer}>
+                    <Image
+                      style={Styles.image}
+                      source={npcData[record.npcID].img}
+                    />
+                  </View>,
+                  <View key="info" style={Styles.infoContainer}>
+                    <View style={Styles.nameContainer}>
+                      <Text style={Styles.name}>
+                        {npcData[record.npcID].name}
+                      </Text>
+                    </View>
+                    <View style={Styles.linesContainer}>
+                      <Text>{record.line}</Text>
+                    </View>
+                    <View style={Styles.timeContainer}>
+                      <Text>
+                        {moment(
+                          record.time.seconds * 1000 +
+                            record.time.nanoseconds / 1000000,
+                        ).format('HH:mm:ss')}
+                      </Text>
+                    </View>
+                  </View>,
+                ]}
+              />
+            );
+          }}
+        />
         <TouchableOpacity style={Styles.backButton} onPress={() => pop()}>
           <Text style={Styles.backText}>回到遊戲</Text>
         </TouchableOpacity>
